ef37847555e547ca3e5975e4075ad41d8b30ce38
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-08 19:28:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,11 +68,22 @@ var day = message.createdAt.getDate()
     }
 })
 
+cli             .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
+  .addField('الكود الخاص بك:', message.author.discriminator)
+  .addField("**عدد الايام منذ افتتاح حسابك:**", message.author.createdAt.getDate())
+    .addField("** تم افتتاح حسابك عام:**", message.createdAt.getFullYear())
+        .addField("** عدد الشهور منذ افتتاح حسابك:**", message.createdAt.getMonth())
+
+  message.channel.send({embed});
+    }
+})
+
 client.on('message', message => {
 if(message.content.startsWith(prefix + "invite")) { 
 message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2080374975`);
 }
 });
+
 
 
 client.on('message', message => {
@@ -88,109 +99,10 @@ if (message.content.startsWith(prefix + 'ms7')) {
   });
 
 
-client.on('message',function(message) {
-    let messageArray = message.content.split(' ');
-    let muteRole = message.guild.roles.get('اي دي الرتبة') || message.guild.roles.find('name', 'Muted');
-    let muteMember = message.mentions.members.first();
-    let muteReason = messageArray[2];
-    let muteDuration = messageArray[3];
-   if(message.content.startsWith(prefix + "muted")) {
-       if(!muteRole) return message.guild.createRole({name: 'Muted'}).then(message.guild.channels.forEach(chan => chan.overwritePermissions(muteRole, {SEND_MESSAGES:false,ADD_REACTIONS:false})));
-       if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة``');
-       if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة مني``');
-       if(!muteMember) return message.channel.send('ℹ **Error:** ``منشن شخص``');
-       if(!muteReason) return message.channel.send('ℹ **Error:** ``حدد سباّ``');
-       if(!muteDuration) return message.channel.send('ℹ **Error:** ``حدد وقت زمني``');
-       if(!muteDuration.match(/[1-7][s,m,h,d,w]/g)) return message.channel.send('ℹ **Error:** ``حدد وقت زمني صحيح``');
-       message.channel.send(`:white_check_mark: **تم اعطاء العضو ميوت : ${muteMember}**`);
-       muteMember.addRole(muteRole);
-       muteMember.setMute(true)
-       .then(() => { setTimeout(() => {
-           muteMember.removeRole(muteRole)
-           muteMember.setMute(false)
-       }, mmss(muteDuration));
-       });
-   } 
-});
 
 
- client.on('message' , message => {
-  if(message.author.bot) return;
-  if(message.content.startsWith(prefix + "rolemsg")) {
-    let args = message.content.split(" ").slice(1);
-
-    if(!args[0]) {
-      message.channel.send("قم بمنشنة الرتبة | *bcrole @everyone رساله");
-        return;
-    }
-    if(!args[1]) {
-      message.channel.send("قم بمنشنة الرتبة | *bcrole @everyone رساله");
-        return;
-    }
-
-      if(args[0] == "@everyone") {
-        message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.memberCount} اعضاء`);
-        message.guild.members.forEach(mi => {
-          mi.send(
-          "الرسالة :" + "\n" +
-         "**" + `${args[1]}` + "**"
-          );
-        });
-        return;
-      }
-          var role = message.mentions.roles.first();
-            if(!role) {
-              message.reply("لا توجد رتبة بهذا الاسم");
-                return;
-            }
-        message.guild.members.filter(m => m.roles.get(role.id)).forEach(sa => {
-        sa.send(
-          "الرسالة :" + "\n" +
-        "**" + `${args[1]}` + "**"
-          );
-        });
-      message.channel.send(`**لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عظو**`);
-    }
-});
 
 
-js 
-const Discord = require("discord.js"); 
-const client = new Discord.Client(); 
-const YTDL = require("ytdl-core"); 
-function play(connection, message) { 
-    var server = servers[message.guild.id]; 
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"})); 
-    server.queue.shift(); 
-    server.dispatcher.on("end", function() { 
-        if(server.queue[0]) play(connection); 
-        else connection.disconnect(); 
-    }) 
-} 
-var servers = {}; 
-client.on('message' , async (message) => { 
-       if(message.content.startsWith(prefix + "plink")) { 
-              let args = message.content.split(" ").slice(1); 
-    //play 
-    if (!args[0]) { 
-         message.channel.send("Please specify a link"); 
-         return 
-    } 
-    if(!message.member.voiceChannel) { 
-        message.channel.sned("I think it may work better if you are in a voice channel!"); 
-    } 
-    if(!servers[message.guild.id]) servers[message.guild.id] = { 
-        queue: [] 
-    } 
-    var server = servers[message.guild.id]; 
-    server.queue.push(args[0]); 
-    message.channel.send("Your song of choice is on the queue.` ") 
-    if(!message.member.voiceConnection) message.member.voiceChannel.join().then(function(connection) { 
-        play(connection, message); 
-    }) 
-} 
-
-});
 
 
 client.on('message', message => {
