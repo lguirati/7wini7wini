38bc68bfc16f7ba164dbe25eafc2f9a751f86750
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-11 18:35:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -153,7 +153,35 @@ if (message.content.startsWith(prefix + 'ms7')) {
 
 
 
+client.on('message', function (message) {
+    var messageParts = message.content.split(' ');
 
+    var command = messageParts[0].toLowerCase();
+    var parameters = messageParts.splice(1, messageParts.length);
+
+
+    switch (command) {
+        case "+join":
+        if(message.guild.voiceConnection){
+            message.reply('I'm Already In A Voice Connection!');
+        }else if(!message.member.voiceChannel){
+            message.reply('You're Not In A Voice Channel!');
+        }else{
+    let channel = message.member.voiceChannel;
+    channel.join();
+        }
+            break;
+case "+play":
+        if(!message.guild.voiceConnection){
+            message.reply('I'm Not In A Voice Channel!');
+        }else{
+//كود بدء الموسيقى مالك
+        }
+            var voiceConnection = client.voiceConnections.first();
+
+            break;
+}
+});
 
 
 
