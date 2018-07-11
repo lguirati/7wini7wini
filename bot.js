359ea432026722d763f546c94c9d07697bc02346
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-11 19:49:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -152,14 +152,25 @@ if (message.content.startsWith(prefix + 'ms7')) {
   });
 
 
+client.on('message', message => { if (message.author.bot) return; if (message.content === prefix + "help") { 		 message.channel.send('**The Message Was Sent On Private**'); 	 		 message.author.sendMessage(` ** __~~The ARAB GAMER ~~__ By: Spomi : 9954 
 
-
+╔[❖════════════❖]╗ Prefix = ' + ' 
    
+╔[❖════════════❖]╗بعض أوامر
 
++bc1 : إرسال رسالة في الخاص للجميع
++rolemsg <@role> يرسل رسالة في الخاص للرول الدي قمت بمنشنته
+متال: 
++rolemsg @Vip كيف حالكم 
++ms7 : مسح الشاة بالعدد
++clear : مسح الشات كليا
++invite : يرسل لك في الخاص رابط دعوته
++avatar : يعرض صورة حسابك 
 
-
-
-
+جاري التعديل على البوت 
+لدعوة البوت من هنا 
+https://discordapp.com/oauth2/authorize?client_id=458656092650471435&scope=bot&permissions=2080374975
+================================================================== `); } });
 
 
 
