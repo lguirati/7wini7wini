07261dadf1107ea638d189efe6f5250b3db2fed5
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-11 21:43:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -174,7 +174,26 @@ https://discordapp.com/oauth2/authorize?client_id=458656092650471435&scope=bot&p
 
 
 
+’’’
+client.on('ready', function(){
+    client.user.setStatus("dnd");
+    var ms = 5000 ;
+    var setGame = [+help For more information ,For add +invite ];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],http://www.twitch.tv/KiNg66S);
+    }, ms);5000
 
+});
+‘’’
 
 
 
