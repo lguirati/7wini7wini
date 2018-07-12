00790bb6e6e3b4d719cd4bc9c0813ab453d455e3
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-12 03:49:57 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -29,8 +29,22 @@ client.user.setGame(`.help`,"http://twitch.tv/S-F")
 });
 
 
+client.on('message', message => { if (message.author.bot) return; if (message.content === prefix + "help") { 		 message.channel.send('**The Message Was Sent On Private**'); 	 		 message.author.sendMessage(` ** __~~The ARAB GAMER ~~__ By: Spomi : 9954 
+╔[❖════════════❖]╗ Prefix =  +
+   
+╔[❖════════════❖]╗بعض أوامر
++bc1 : إرسال رسالة في الخاص للجميع
++rolemsg <@role> يرسل رسالة في الخاص للرول الدي قمت بمنشنته
+متال: 
++rolemsg @Vip كيف حالكم 
++ms7 : مسح الشاة بالعدد
++clear : مسح الشات كليا
++invite : يرسل لك في الخاص رابط دعوته
++avatar : يعرض صورة حسابك 
+جاري التعديل على البوت 
+لدعوة البوت من هنا 
 
-
+https://discordapp.com/oauth2/authorize?client_id=466684487674363904&scope=bot&permissions=2080374975
 
 
 
