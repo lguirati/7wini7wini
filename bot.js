c32d152529939bd63e9a6bb84ebbea5196f42ca1
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-13 18:03:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -176,6 +176,19 @@ https://discordapp.com/oauth2/authorize?client_id=458656092650471435&scope=bot&p
 
 
 
+client.on('message', message => {
+       if (message.content.startsWith(prefix + 'botserver')) {
+     let msg =  client.guilds.map(guild => `**${guild.name}** عدد الاعضاء: ${guild.memberCount}`).join('\n');
+  let embed = new Discord.RichEmbed()
+  .setTitle(`${client.guilds.size}سيرفرات `)
+  .setDescription(`${msg}`)
+  .setColor("#ebf442");
+  message.channel.send(embed);
+}
+});
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
