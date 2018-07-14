37a4f50526101d86df0d8e0099aefd4b607322ff
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-14 01:25:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -183,26 +183,34 @@ client.on('message', message => {
 }
 });
 
+
+
 client.on('message', message => {
-if (message.content === "$id") {
-var year = message.createdAt.getFullYear()
-var month = message.createdAt.getMonth()
-var day = message.createdAt.getDate()
-     let embed = new Discord.RichEmbed()
-     .setAuthor(message.author.username, message.author.avatarURL)
-  .setThumbnail(message.author.avatarURL)
-    .addField("**اسمك:**",  '**[ ' + `${message.author.username}` + ' ]**')
-      .setThumbnail(message.author.avatarURL)
-               .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
-  .addField('الكود الخاص بك:', message.author.discriminator)
-  .addField("**عدد الايام منذ افتتاح حسابك:**", message.author.createdAt.getDate())
-    .addField("** تم افتتاح حسابك عام:**", message.createdAt.getFullYear())
-        .addField("** عدد الشهور منذ افتتاح حسابك:**", message.createdAt.getMonth())
+    if (message.content.startsWith("رابط")) {
 
-  message.channel.send({embed});
-    }
-})
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(" ✅    تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
 
+-${message.guild.name}  Link
+**`)
+      message.author.sendEmbed(Embed11)
+    }
+});
+ 
 
 
 
