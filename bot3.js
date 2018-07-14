17a8f65d89421a737147b077806427e955c9be5f
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-14 03:59:43 +0100

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -29,81 +29,37 @@ client.user.setGame(`×help`,"http://twitch.tv/S-F")
 });
 
                       
-                 client.on('message', msg => {
-var prefix = "×";
-  if(!msg.guild) return;
-    if(!msg.member.hasPermission('MANAGE_CHANNELS')) return message.reply('**⚠ لا يوجد لديك صلاحية**');
-    if (msg.content.startsWith(prefix +'sd')) {
-let ra3d = new Discord.RichEmbed()
-.setColor('RANDOM')
-.setThumbnail(msg.author.avatarURL)
-.setDescription(`هل انت متاكد من مسح كل شي بالسيرفر ؟\n  ✅  \n  ❌ \n  لديك 60 ثانية للاختيار`)                                                                                                                                                                       
-msg.channel.send(ra3d).then(message => {
- message.react('✅').then(r=>{
- message.react('❌').then(r=>{           
- let sd = (reaction, user) => reaction.emoji.name === '✅' && user.id === msg.author.id;
- let nd = (reaction, user) => reaction.emoji.name === '❌' && user.id === msg.author.id;
- let ds  = message.createReactionCollector(sd, { time: 60000 });
- let dn  = message.createReactionCollector(nd, { time: 60000 });
-dn.on("collect", r => {
-msg.channel.send("`تم الالغاء`")
-message.delete();
-})
-ds.on("collect", r => {
-     message.guild.channels.forEach(c => { c.delete() })
-     message.guild.createChannel('general', 'text').then(c=> c.send(ra3d));
-     let ra3d = new Discord.RichEmbed()
-            .setColor('#fd0101')
-            .setDescription('`تم حذف كل شي في السيرفر✅`')
-           message.channel.sendEmbed(ra3d);
-})
-})
-})
-})
+        
+client.on('message', function (message) {
+    var messageParts = message.content.split(' ');
+
+    var command = messageParts[0].toLowerCase();
+    var parameters = messageParts.splice(1, messageParts.length);
+
+
+    switch (command) {
+        case "×join":
+        if(message.guild.voiceConnection){
+            message.reply('I\'m Already In A Voice Connection!');
+        }else if(!message.member.voiceChannel){
+            message.reply('You\'re Not In A Voice Channel!');
+        }else{
+    let channel = message.member.voiceChannel;
+    channel.join();
+        }
+            break;
+case "×play":
+        if(!message.guild.voiceConnection){
+            message.reply('I\'m Not In A Voice Channel!');
+        }else{
+//كود بدء الموسيقى مالك
+        }
+            var voiceConnection = client.voiceConnections.first();
+
+            break;
 }
 });
-
-
-client.on('message', message => {
-    if (message.content === "+serooms") {
-    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
-            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
-
-        
-     message.guild.createChannel('「 O W N E R 」', 'voice')
-     message.guild.createChannel('「 C O - L E A D E R 」', 'voice')
-     message.guild.createChannel('「ADMINSTRATOR」', 'voice')
-     message.guild.createChannel('𖦲₁PARTY | بارتي𖦲', 'voice')
-     message.guild.createChannel('𖦲₂PARTY | بارتي𖦲', 'voice')
-     message.guild.createChannel('𖦲₂PARTY | بارتي𖦲', 'voice')
-     message.guild.createChannel('✬ʝuşτ-1✬', 'voice')
- message.guild.createChannel('✬ʝuşτ-2✬', 'voice')
-     message.guild.createChannel('✬ʝuşτ-3✬', 'voice')
-     message.guild.createChannel('✬ʝuşτ-4✬', 'voice')
-     message.guild.createChannel('✬ʝuşτ-5✬', 'voice')
-     message.guild.createChannel('😴sleep', 'voice')
-          message.guild.createChannel('༆كَبّـآرَ آلَشّـخٌـصِـيّآتُ༆', 'voice')
-     message.guild.createChannel('welcome', 'text')
-     message.guild.createChannel('info', 'text')
-     message.guild.createChannel('bot', 'text')
-     message.guild.createChannel('chat', 'text')
-     message.guild.createChannel('Youtube', 'text')
-     message.guild.createChannel('bo7', 'text')
-     message.guild.createChannel('party', 'text')
-     message.guild.createChannel('pic', 'text')
-
-
-message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم صناعة السيرفر**')
-}
-});
-
-
-client.on('message', message => {
-if(message.content.startsWith(prefix + "invite")) { 
-message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2080374975`);
-}
-});
-
+ 
 
 
 
