8958876ec706f798b04065d25921867b29916056
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-17 00:58:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -272,6 +272,50 @@ message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم 
 }
 });
  
+client.on('message', function (message) {
+    var messageParts = message.content.split(' ');
+
+    var command = messageParts[0].toLowerCase();
+    var parameters = messageParts.splice(1, messageParts.length);
+
+
+    switch (command) {
+        case "$join":
+        if(message.guild.voiceConnection){
+            message.reply('I'm Already In A Voice Connection!');
+        }else if(!message.member.voiceChannel){
+            message.reply('You're Not In A Voice Channel!');
+        }else{
+    let channel = message.member.voiceChannel;
+    channel.join();
+        }
+            break;
+case "$play":
+        if(!message.guild.voiceConnection){
+            message.reply('I'm Not In A Voice Channel!');
+        }else{
+//كود بدء الموسيقى مالك
+        }
+            var voiceConnection = client.voiceConnections.first();
+
+            break;
+}
+});
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
 
 
