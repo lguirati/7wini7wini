8d5c13487053c1eb10a7f9e8bf349e083ad24008
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-17 15:07:19 +0100

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -68,7 +68,22 @@ client.on('message', Codes => {
 }
 });
  
-
+client.on('message', message => {
+    if (message.content.startsWith("×avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
 
   
 
