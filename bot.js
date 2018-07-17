d3315a81d014f8b5f9ed3773bba560e8b73ae0ca
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-17 01:09:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -272,6 +272,8 @@ message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم 
 }
 });
  
+
+
 client.on('message', function (message) {
     var messageParts = message.content.split(' ');
 
@@ -282,9 +284,9 @@ client.on('message', function (message) {
     switch (command) {
         case "$join":
         if(message.guild.voiceConnection){
-            message.reply('I'm Already In A Voice Connection!');
+            message.reply('I\'m Already In A Voice Connection!');
         }else if(!message.member.voiceChannel){
-            message.reply('You're Not In A Voice Channel!');
+            message.reply('You\'re Not In A Voice Channel!');
         }else{
     let channel = message.member.voiceChannel;
     channel.join();
@@ -292,7 +294,7 @@ client.on('message', function (message) {
             break;
 case "$play":
         if(!message.guild.voiceConnection){
-            message.reply('I'm Not In A Voice Channel!');
+            message.reply('I\'m Not In A Voice Channel!');
         }else{
 //كود بدء الموسيقى مالك
         }
