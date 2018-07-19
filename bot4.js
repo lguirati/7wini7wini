389d67ff8fc38ae6d23cefa5087785fb29b84df0
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-19 19:44:32 +0100

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -54,27 +54,42 @@ client.channels.find('id', '453449558761144330').setName("Welcome To SERVER");
 
 client.on("ready", async  => {
 setInterval(function(){
-client.channels.find('id', '465574494476435461').setName("W");
-client.channels.find('id', '465574494476435461').setName("We");
-client.channels.find('id', '465574494476435461').setName("Wel");
-client.channels.find('id', '465574494476435461').setName("Welc");
-client.channels.find('id', '465574494476435461').setName("Welco");
-client.channels.find('id', '465574494476435461').setName("Welcom");
-client.channels.find('id', '465574494476435461').setName("Welcome");
-client.channels.find('id', '465574494476435461').setName("Welcome T");
-client.channels.find('id', '465574494476435461').setName("Welcome To");
-client.channels.find('id', '465574494476435461').setName("Welcome To ");
-client.channels.find('id', '465574494476435461').setName("Welcome To S");
-client.channels.find('id', '465574494476435461').setName("Welcome To SE");
-client.channels.find('id', '465574494476435461').setName("Welcome To SER");
-client.channels.find('id', '465574494476435461').setName("Welcome To SERV");
-client.channels.find('id', '465574494476435461').setName("Welcome To SERVE");
-client.channels.find('id', '465574494476435461').setName("Welcome To SERVER");
+client.channels.find('id', '465574494476435461').setName("a");
+client.channels.find('id', '465574494476435461').setName("an");
+client.channels.find('id', '465574494476435461').setName("ann");
+client.channels.find('id', '465574494476435461').setName("anno");
+client.channels.find('id', '465574494476435461').setName("annou");
+client.channels.find('id', '465574494476435461').setName("announ");
+client.channels.find('id', '465574494476435461').setName("announc");
+client.channels.find('id', '465574494476435461').setName("announce");
+client.channels.find('id', '465574494476435461').setName("announcem");
+client.channels.find('id', '465574494476435461').setName("announceme");
+client.channels.find('id', '465574494476435461').setName("announcemen");
+client.channels.find('id', '465574494476435461').setName("announcement");
 }, 7000);
  
 });
 
 
+
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '444855713726988288').setName("🔴");
+client.channels.find('id', '444855713726988288').setName("🔴l");
+client.channels.find('id', '444855713726988288').setName("🔴li");
+client.channels.find('id', '444855713726988288').setName("🔴liv");
+client.channels.find('id', '444855713726988288').setName("🔴live");
+client.channels.find('id', '444855713726988288').setName("🔴live-");
+client.channels.find('id', '444855713726988288').setName("🔴live-t");
+client.channels.find('id', '444855713726988288').setName("🔴live-tw");
+client.channels.find('id', '444855713726988288').setName("🔴live-twi");
+client.channels.find('id', '444855713726988288').setName("🔴live-twit");
+client.channels.find('id', '444855713726988288').setName("🔴live-twitc");
+client.channels.find('id', '444855713726988288').setName("🔴live-twitch");
+client.channels.find('id', '444855713726988288').setName("🔴live-twitch🔴");
+}, 7000);
+ 
+});
 
 
 
