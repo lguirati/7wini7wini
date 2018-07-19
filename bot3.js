957e7960a944d19370c7ad1335993b710faf72bc
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-19 13:50:07 +0100

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -50,145 +50,30 @@ client.on('message', message => {
 
   
 
-client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('468777690191888384');
-  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
-  const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`-lg Voice Online -[${currentSize}]`);
-  if (currentSize !== size) channel.setName(`-lg Voice Online -[${currentSize}]`);
-});      
 
 
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '469471231427477505').setName("W");
+client.channels.find('id', '469471231427477505').setName("We");
+client.channels.find('id', '469471231427477505').setName("Wel");
+client.channels.find('id', '469471231427477505').setName("Welc");
+client.channels.find('id', '469471231427477505').setName("Welco");
+client.channels.find('id', '469471231427477505').setName("Welcom");
+client.channels.find('id', '469471231427477505').setName("Welcome");
+client.channels.find('id', '469471231427477505').setName("Welcome T");
+client.channels.find('id', '469471231427477505').setName("Welcome To");
+client.channels.find('id', '469471231427477505').setName("Welcome To ");
+client.channels.find('id', '469471231427477505').setName("Welcome To S");
+client.channels.find('id', '469336127254298625').setName("Welcome To SE");
+client.channels.find('id', '469471231427477505').setName("Welcome To SER");
+client.channels.find('id', '469471231427477505').setName("Welcome To SERV");
+client.channels.find('id', '469471231427477505').setName("Welcome To SERVE");
+client.channels.find('id', '469471231427477505').setName("Welcome To SERVER");
+}, 3000);
+ 
 });
-/*جميع الحقوق محفوظهه لريبل ولسيرفر كودز
-رآح يرسل للأونر تحذير + م يتطلب ملفات سويته لكم داتا مؤقت
-سو روم بأسم log 
-أو غيره من الكود عشان يرسل هنا التحذير
-nvm i 10 
-nvm use 10
-npm i discord.js
-*/
-var guilds = {};
-client.on('guildBanAdd', function(guild) {
-            const rebellog = client.channels.find("name", "log"),
-            Onumber = 3,
-  Otime = 10000
-guild.fetchAuditLogs({
-    type: 22
-}).then(audit => {
-    let banner = audit.entries.map(banner => banner.executor.id)
-    let bans = guilds[guild.id + banner].bans || 0 
-    guilds[guild.id + banner] = {
-        bans: 0
-    }
-      bans[guilds.id].bans += 1; 
-if(guilds[guild.id + banner].bans >= Onumber) {
-try {
-let roles = guild.members.get(banner).roles.array();
-guild.members.get(banner).removeRoles(roles);
-  guild.guild.member(banner).kick();
-
-} catch (error) {
-console.log(error)
-try {
-guild.members.get(banner).ban();
-  rebellog.send(`<@!${banner.id}>
-حآول العبث بالسيرفر @everyone`);
-guild.owner.send(`<@!${banner.id}>
-حآول العبث بالسيرفر ${guild.name}`)
-    setTimeout(() => {
- guilds[guild.id].bans = 0;
-  },Otime)
-} catch (error) {
-console.log(error)
-}
-}
-}
-})
-});
- let channelc = {};
-  client.on('channelCreate', async (channel) => {
-  const rebellog = client.channels.find("name", "log"),
-  Oguild = channel.guild,
-  Onumber = 3,
-  Otime = 10000;
-  const audit = await channel.guild.fetchAuditLogs({limit: 1});
-  const channelcreate = audit.entries.first().executor;
-  console.log(` A ${channel.type} Channel called ${channel.name} was Created By ${channelcreate.tag}`);
-   if(!channelc[channelcreate.id]) {
-    channelc[channelcreate.id] = {
-    created : 0
-     }
- }
- channelc[channelcreate.id].created += 1;
- if(channelc[channelcreate.id].created >= Onumber ) {
-    Oguild.members.get(channelcreate.id).kick();
-rebellog.send(`<@!${channelcreate.id}>
-حآول العبث بالسيرفر @everyone`);
-channel.guild.owner.send(`<@!${channelcreate.id}>
-حآول العبث بالسيرفر ${channel.guild.name}`)
-}
-  setTimeout(() => {
- channelc[channelcreate.id].created = 0;
-  },Otime)
-  });
-
-let channelr = {};
-  client.on('channelDelete', async (channel) => {
-  const rebellog = client.channels.find("name", "log"),
-  Oguild = channel.guild,
-  Onumber = 3,
-  Otime = 10000;
-  const audit = await channel.guild.fetchAuditLogs({limit: 1});
-  const channelremover = audit.entries.first().executor;
-  console.log(` A ${channel.type} Channel called ${channel.name} was deleted By ${channelremover.tag}`);
-   if(!channelr[channelremover.id]) {
-    channelr[channelremover.id] = {
-    deleted : 0
-     }
- }
- channelr[channelremover.id].deleted += 1;
- if(channelr[channelremover.id].deleted >= Onumber ) {
-  Oguild.guild.member(channelremover).kick();
-rebellog.send(`<@!${channelremover.id}>
-حآول العبث بالسيرفر @everyone`);
-channel.guild.owner.send(`<@!${channelremover.id}>
-حآول العبث بالسيرفر ${channel.guild.name}`)
-}
-  setTimeout(() => {
- channelr[channelremover.id].deleted = 0;
-  },Otime)
-  });
-
-
-client.on('message', message => { if (message.author.bot) return; if (message.content === prefix + "help") { 		 message.channel.send('**The Message Was Sent On Private**'); 	 		 message.author.sendMessage(` ** __~~The ARAB GAMER ~~__ By: Spomi : 9954 
-╔[❖════════════❖]╗ Prefix = ×
-creat un room avec le nom look
-creat one room name look 
-سوي روم بإسم look
-
-For add Bot : لدعوت البوت 
-
-https://discordapp.com/oauth2/authorize?client_id=469353155335946240&permissions=8&scope=bot
-
-
-×invite :msg prv avec invite 
-
-https://discordapp.com/oauth2/authorize?client_id=469353155335946240&permissions=8&scope=bot
-
-================================================================== `); } });
-
-
-
-client.on('message', message => {
-if(message.content.startsWith(prefix + "invite")) { 
-message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2080374975`);
-}
-});
-
 
 
 
