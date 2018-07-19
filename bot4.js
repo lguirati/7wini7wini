3d9c8e30b1c80adf7a667d3fca644b017ce502b6
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-19 19:50:00 +0100

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -4,7 +4,7 @@ const prefix = '='
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Come private #9954 `,"http://twitch.tv/S-F")
+client.user.setGame(`For add Talke with #9954`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
