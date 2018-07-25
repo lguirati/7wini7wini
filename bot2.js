a4c9e404e1261be3d9aefb4477f6148b578f4cd3
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-25 03:04:26 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -266,7 +266,7 @@ return;
 });
  
 client.on('message', message => {
-    if (message.content === ".") {
+    if (message.content === ".createrooms") {
     if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
             if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
 
