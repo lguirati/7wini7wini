ac6182c958c744e6b8a5748a1bdb2510d756e69d
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-25 04:17:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,23 +2,37 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '$'
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+
+
+
+
 client.on('ready', function(){
-    var ms = 10000 ;
-    var setGame = [' $help ',' $bc <--Msg for all |  $avatar <--See your pic',' perfix -->$ ','$id',' $invite'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/D.JPEI`);
-    }, ms);
+    client.user.setStatus("dnd");
+    var ms = 5000 ;
+    var setGame = [`AdminBot`,`LEADER GAMES`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);4000
 
 });
+
 
 client.on('message', message => {
 var prefix = "$";
