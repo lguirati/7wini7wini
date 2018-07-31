9d6e29bb7496aaa68f74a9eba7c2fbe38eea4a27
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-31 02:21:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -491,19 +491,6 @@ client.on('voiceStateUpdate', (old, now) => {
 });
 
           
-client.on('guildMemberAdd', member => {
-    const botCount = member.guild.members.filter(m=>m.user.bot).size
-    const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('473656733344989186').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
-    client.channels.get('473656954623885313').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
-});
-
-client.on('guildMemberRemove', member => {
-    const botCount = member.guil.members.filter(m=>m.user.bot).size
-    const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('473656733344989186').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
-    client.channels.get('473656954623885313').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
-});
 
 
 
