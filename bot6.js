47472952792b2b0cd26d252ba0b90dea87318a6d
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-01 00:01:07 +0100

    Create bot6.js

---
+++ bot6.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const client = new Discord.Client();
+const prefix = '+'
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('')
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('')
+  console.log('╚[════════════════════════════════════]╝')
+});
+client.on('ready', () => {
+     client.user.setActivity("PornHub 🙃 MILFAYA ",{type: 'WATCHING'})
+
+});
