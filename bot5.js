bb6a0afe74e31a77b4afd0c3a864a4955ce54ba1
Spomi <40448323+Spomi@users.noreply.github.com>
2018-07-31 21:17:52 +0100

    Update bot5.js

--- bot5.js
+++ bot5.js
@@ -1,28 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '0'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 });
+
+
+
+
+
+
+
+
+
+client.on('ready', function(){
+    client.user.setStatus("dnd");
+    var ms = 5000 ;
+    var setGame = [`أفلت من فضلك قدمي`,`Life is Hell `,` #MiLfaya غنعتازل قهرنا الفقر`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);1000
+
+});
+
+
+
+                          
 
 
 
