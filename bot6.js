faf1d7c40af078b162d50aee912f53ffc8f512ce
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-01 06:40:06 +0100

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
-     client.user.setActivity("🙃كنعطيهم قيمة وكيتيق راسهم حيتش عندهم نقص",{type: 'PLAYING'});
+     client.user.setActivity("🙃تواضعة وحسو براسهم-->عندهم نقص",{type: 'PLAYING'});
 
-});
+})
 
 
 
