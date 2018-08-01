46b60469fc1083f51c31b298da92bc410fbd1006
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-01 03:49:42 +0100

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -1,19 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
+const client = new Discord.Client()
 client.on('ready' , () => {
 client.user.setGame('S A D L i F e 😥 i NEED 💞');
+})
+client.on('message' , mohamed => {
+if(mohamed.content.startsWith('@[PG] Spomi_YT 🌹') {
+mohamed.reply('إدا لم أرد عليك الان يمكنك ترك رسالة في الخاص ربتما أعود');
+}
 });
-
-
-
-
-
-
-
 client.login('BOT6_TOKEN');
