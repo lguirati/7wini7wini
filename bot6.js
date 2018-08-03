3916c8989281c2fa4b9bf3216e74209056972b70
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-03 15:06:37 +0100

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
-     client.user.setActivity("S A D_B O Y|~_~|--> i am fine<3",{type: 'PLAYING'});
+     client.user.setActivity("i am Sad & Alone But i am Fine<3",{type: 'PLAYING'});
 
 })
 
