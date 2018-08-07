e89a9a9631b16044615fc022595ccdccdf899fc4
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-07 15:49:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -525,7 +525,45 @@ Server Count: __${guild.memberCount}__**`)
 });
 
 
+client.on('message', message => {
+  if(!message.channel.guild) return;
+if(message.content.startsWith( 'مسح')) {
+if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You Do not have permission** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let request = `Requested By ${message.author.username}`;
+message.channel.send(`**هل انت متاكد من فعل هذا**`).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
 
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`Chat will delete`).then(m => m.delete(5000));
+var msg;
+       msg = parseInt();
+
+     message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+     message.channel.sendMessage("", {embed: {
+       title: "`` تم المسح ``",
+       color: 0x06DF00,
+       footer: {
+
+       }
+     }}).then(msg => {msg.delete(3000)});
+
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Cتم الايقاف**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+});
 
 
 
