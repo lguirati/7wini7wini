3aa2bad8fd71ddad1ab071ea00ee7f903329eb76
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-15 23:56:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -659,34 +659,6 @@ welcomer.sendFile(canvas.toBuffer())
 });
 
 		
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("453449558761144330");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("404692653469597733");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    setTimeout(function() {
- channel.send(`**invited by** ${Invite.inviter} `) ;
-                    },1500);
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
-});
-
 
 
 
