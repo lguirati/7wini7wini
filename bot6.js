980b8459f1fc2ff721db0d5241ba84ace9b43e83
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-16 13:47:35 +0100

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -9,6 +9,21 @@ client.on('ready', () => {
 
 
 
+async def on_server_join(self, server):
+        await self.wait_until_ready()
+        embed = discord.Embed()
+        embed.title = 'SERVER JOIN'
+        embed.description = 'Shard ID: {0}'.format(self.shard_id)
+        embed.set_author(name='{0} <{0.id}>'.format(server.owner), icon_url=server.owner.avatar_url)
+        embed.add_field(name='Server', value='{0.name} <{0.id}>'.format(server))
+        embed.add_field(name='Members', value='{0}/{1}'.format(sum(1 for x in server.members if x.status == discord.Status.online or x.status == discord.Status.idle), len(server.members)))
+        embed.add_field(name='Default Channel', value=server.default_channel)
+        embed.add_field(name='Channels', value='Text: {0}\nVoice: {1}\nTotal: {2}'.format(sum(1 for x in server.channels if x.type == discord.ChannelType.text), sum(1 for x in server.channels if x.type == discord.ChannelType.voice), len(server.channels)))
+        embed.color = discord.Color.green()
+        embed.timestamp = datetime.datetime.now()
+        await self.queue_message('channel ID', embed)
+ 
+
 
 
 client.login(process.env.BOT6_TOKEN);
