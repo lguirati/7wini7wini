7debf17de4858acc34e4c325602bd662756a5118
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-17 13:19:13 +0100

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -401,7 +401,7 @@ const w = ['./w1.png'];
                         ctx.fontSize = '20px';
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
-                        ctx.fillText(`welcome to Brix`, 300, 130);
+                        ctx.fillText(`welcome to Army`, 300, 130);
                        
                         ctx.font = "bold 12px Arial";
                         ctx.fontSize = '20px';
