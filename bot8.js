0691b49af283b3d98f903482f570b8c47ead271b
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-29 17:31:18 +0100

    Update bot8.js

--- bot8.js
+++ bot8.js
@@ -26,6 +26,26 @@ client.on('ready', () => {
     client.user.setStatus("dnd")
 });
 //by ! - .'
+
+client.on('ready', function(){
+    var ms = 5000 ;
+    var setGame = [`${prefix}help Servers ${client.guilds.size} `,`${prefix}invite Users ${client.users.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/osama_gmt`);
+    }, ms);5000
+
+});
+
+
 const prefix = "#"
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
@@ -259,23 +279,6 @@ client.on('message', message => {
 
 
 
-client.on('ready', function(){
-    var ms = 5000 ;
-    var setGame = [`${prefix}help Servers ${client.guilds.size} `,`${prefix}invite Users ${client.users.size}`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/osama_gmt`);
-    }, ms);5000
-
-});
 
 
 client.on('message', message => {
