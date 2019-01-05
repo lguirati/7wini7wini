32f77e7816a37cf45a5e5d55d35c6fe59ba0590d
Spomi <40448323+Spomi@users.noreply.github.com>
2019-01-05 11:59:59 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -686,44 +686,6 @@ var prefix = "$";
   }
  });
  
- const yourID = "281762317522305024";
-const setupCMD = "1roles"
-let initialMessage = `**@everyone  rolereact**
-**React to the messages below to get role. If you would like to remove the role remove your reaction!** `;
-const roles = ["GAY", "KID", "+18", "16-17", "funny", "mcharml", "3nab", "bitch", "3ami9", "m3wa9"];//رتب
-const reactions = ["👬", "👶", "😊", "🔞", "😂", "🤙", "🍇", "💋", "🍩", "🙅"];//رياكشن
-if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
-function generateMessages(){
-    var messages = [];
-    messages.push(initialMessage);
-    for (let role of roles) messages.push(`React below to get the **"${role}"** role!`); 
-    return messages;
-}
-client.on("message", message => {
-    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
-        var toSend = generateMessages();
-        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
-        for (let mapObj of mappedArray){
-            message.channel.send(mapObj[0]).then( sent => {
-                if (mapObj[1]){
-                  sent.react(mapObj[1]);  
-} });}}})
-client.on('raw', event => {
-    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
-        let channel = client.channels.get(event.d.channel_id);
-        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
-        let user = msg.guild.members.get(event.d.user_id);
-        if (msg.author.id == client.user.id && msg.content != initialMessage){
-            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
-            var role = msg.content.match(re)[1];
-            if (user.id != client.user.id){
-                var roleObj = msg.guild.roles.find('name', role);
-                var memberObj = msg.guild.members.get(user.id);
-      if (event.t === "MESSAGE_REACTION_ADD"){
-       memberObj.addRole(roleObj)
-     } else {
-     memberObj.removeRole(roleObj);
-}} }})}});
  
 
 
