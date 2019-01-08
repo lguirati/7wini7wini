15c15438993d51fe2cd1b9203f64f74fcb05933e
Spomi <40448323+Spomi@users.noreply.github.com>
2019-01-08 22:27:49 +0000

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -32,15 +32,15 @@ client.on('ready', function(){
 client.on('guildMemberAdd', member => {
     const botCount = member.guild.members.filter(m=>m.user.bot).size
     const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('469336122510802944').setName(`⟫『 ${memberCount} USERS 』⟪`);
-    client.channels.get('469336122762461186').setName(`⟫『 ${botCount} THE BOT 』⟪`);
+    client.channels.get('532324212619083785').setName(`⟫『 ${memberCount} USERS 』⟪`);
+    client.channels.get('532324212619083785').setName(`⟫『 ${botCount} THE BOT 』⟪`);
 });
 
 client.on('guildMemberRemove', member => {
     const botCount = member.guild.members.filter(m=>m.user.bot).size
     const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('469336122510802944').setName(`⟫『 ${memberCount} USERS 』⟪`);
-    client.channels.get('469336122762461186').setName(`⟫『 ${botCount} THE BOT 』⟪`);
+    client.channels.get('532324442525794324').setName(`⟫『 ${memberCount} USERS 』⟪`);
+    client.channels.get('532324442525794324').setName(`⟫『 ${botCount} THE BOT 』⟪`);
 });
 
 
