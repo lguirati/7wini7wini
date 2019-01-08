b316348f242bafc4e0af1c04cf0e92e7dcd82e95
Spomi <40448323+Spomi@users.noreply.github.com>
2019-01-08 22:47:38 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -665,8 +665,8 @@ client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('531620768589807616');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
-  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
+  if (!size) return channel.setName(`ᴠᴏɪᴄᴇ ᴏɴʟɪɴᴇ: ${currentSize}`);
+  if (currentSize !== size) channel.setName(`ᴠᴏɪᴄᴇ ᴏɴʟɪɴᴇ: ${currentSize}`);
 });
 
 
