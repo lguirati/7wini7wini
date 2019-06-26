30d23cd7a9a3797c6a086fb76294c68018cee19d
lguirati <yassinelguirati2001@gmail.com>
2019-06-26 19:21:14 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -436,29 +436,6 @@ Server Count: __${guild.memberCount}__**`)
   
 
 
-client.on('message', msg => {
-    if(msg.author.bot) return;
-    
-    if(msg.content === '.seruser') {
-      client.guilds.forEach(g => {
-        
-        let l = g.id
-        g.channels.get(g.channels.first().id).createInvite({
-          maxUses: 5,
-          maxAge: 86400
-        }).then(i => msg.channel.send(`
-        **
-        Invite Link : <https://discord.gg/${i.code}>
-        Server : ${g.name} | Id : ${g.id} 
-        Owner ID : ${g.owner.id}
-        **
-        `))
-  
-  
-      })
-    }
-    
-  })
 
 
 client.on('message', message => {
